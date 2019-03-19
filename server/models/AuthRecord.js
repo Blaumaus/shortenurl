3439c9684c97d97dc42021cf334f73b1fe87f18b
Vladimir Bel <cgartscorp@gmail.com>
2019-03-19 11:12:19 +0500

    Corrections model authRecord

--- server/models/AuthRecord.js
+++ server/models/AuthRecord.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const UrlRecords = new Schema({
+const AuthRecords = new Schema({
     username: String,
     password: String,
     email: String,
 });
-module.exports = mongoose.model('Record', UrlRecords);
+module.exports = mongoose.model('Record', AuthRecords);
