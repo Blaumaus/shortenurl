3796e9c802c73d72d1abf23456b95bf8c9bae35a
Vladimir Bel <cgartscorp@gmail.com>
2019-03-20 12:29:33 +0500

    Corrections.

--- server/models/RegisterRecord.js
+++ server/models/RegisterRecord.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const AuthRecordSchema = new Schema({
+const RegisterRecordSchema = new Schema({
     email: String,
     passwordHash: String,
     firstName: String,
     lastName: String,
     ipAddress: String,
 });
-module.exports = mongoose.model('AuthRecord', AuthRecordSchema);
+module.exports = mongoose.model('RegisterRecord', RegisterRecordSchema);
