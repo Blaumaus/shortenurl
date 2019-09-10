a54750ff065d28e907f000c7f2d0b286ab804220
Andrew R <35810911+Gramatik@users.noreply.github.com>
2019-09-10 23:14:48 +0300

    Redirection big fix (???)
    Removed old badcoded async/await style and changed in for classic promises =)

--- server/routes/api/redirector.js
+++ server/routes/api/redirector.js
@@ -7,37 +7,17 @@ require('dotenv').config()
 // @route   GET api/records/:url
 // @desc    Redirect to long URL
 // @access  Public
-router.get('/:url', async (req, res) => {
-    // Init long_url variable
-    let long_url
-
-    // Make a request to DB to increment clicks on URL
-    /*await*/ UrlRecord.findOne({"shortUrn": req.params.url}, (err, found_obj) => {
-        // Check for error
-        if (err) {
-            console.log(`[ERROR] DB error: ${err}`)
-            res.json({ state: "error", message: "DB error." }).end()
-        } else {
-            if (found_obj === null || found_obj.length < 0) {
-                console.log(`[ERROR] URL not found error: ${err}`)
-                res.json({ state: "error", message: "URL not found." }).end()
-            } else {
-                long_url = found_obj.longUrl
-                found_obj.clicks += 1
-                // Save updated click
-                found_obj.save((err, updated_obj) => {
-                    if (err) {
-                        console.log(`[ERROR] Counter update error: ${err}`)
-                        res.json({ state: "error", message: "Counter update error." }).end()
-                    }
-                })
-            }
-        }
+router.get('/:url', (req, res) => {
+  // Didn't even run this code, do a PR if it doesn't work, but I guess it does
+  UrlRecord.findOne({ 'shortUrn': req.params.url })
+    .then(result => {
+      if (result !== null) {
+        UrlRecord.updateOne({ '_id': result._id }, result.clicks += 1)
+          .then(save_result => res.status(301).redirect(result.longUrl))
+          .catch(e => result.status(500).json({ state: 'error', message: 'Database error :/ Reclick this link later =)' }))
+      } else result.status(404).json({ state: 'error', message: 'URL not found, better luck next time.' })
     })
-
-    // Redirect to long URL
-    if (long_url) res.status(301).redirect(long_url)
-    else res.json({ state: "error", message: "URL not found." }).end()
+    .catch(e => result.status(500).json({ state: 'error', message: 'Database error :/ Possibly we are under DDOS attack or the admin shitcoded again' }))
 })
 
 // Export
